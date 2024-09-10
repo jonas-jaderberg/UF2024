490fc79885ed3de0c145ee0775de7b4c676ecf00
jonas-jaderberg <75827629+jonas-jaderberg@users.noreply.github.com>
2024-09-10 12:04:50 +0000

    sh file and .net 6

---
+++ 2-1 Hand on for developers/Demo DotNetCore/Schema/generate_pull_server_svc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wsdlpath=""
+exschemapath=""
+schemapath=""
+"dotnet-svcutil" \
+$wsdlpath"SnapshotPull.wsdl" \
+    -v Debug \
+    --sync \
+    --noStdLib \
+    --serializer XmlSerializer \
+    --outputFile SnapshotPullSvc.cs \
+    --namespace "*,DatexII" \
+    "$exschemapath""DATEXII_3_MessageContainer.xsd" \
+    "$exschemapath""DATEXII_3_ExchangeInformation.xsd" \
+    "$schemapath""DATEXII_3_Common.xsd" \
+    "$schemapath""DATEXII_3_D2Payload.xsd" \
+    "$schemapath""DATEXII_3_LocationReferencing.xsd" \
+    "$schemapath""DATEXII_3_RoadTrafficData.xsd" \
+    "$schemapath""DATEXII_3_Situation.xsd"
